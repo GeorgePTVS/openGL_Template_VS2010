c9df32bdbd0c69695683d98e30ca1ea52d2128ef
GeorgePTVS <gpolchin@truevisionsys.com>
2014-11-30 13:09:12 -0800

    change width and height when window gets resized

---
+++ webroot/app/window.js
@@ -0,0 +1,31 @@
+function onWindowResize()
+{
+  var winWidth  = $( window ).width();
+  var winHeight = $( window ).height();
+  
+  // set main_content width rel to window and height rel to width
+  var MAIN_W_SCALAR = 0.7;
+  var mainWidth  = MAIN_W_SCALAR * winWidth;
+  var mainHeight = mainWidth;
+
+  // if ( !oneTimeOnly )
+    // alert('onWindowResize. winWidth = ' + winWidth + ' winHeight = ' + winHeight + ' mainWidth; ' + mainWidth + ' mainHeight: ' + mainHeight);
+    
+  // var oneTimeOnly = true;  
+  
+  // have to account for aspect ratio.  
+  if ( winWidth < winHeight )
+  {
+    mainWidth = MAIN_W_SCALAR * winWidth;
+    mainHeight = mainWidth;
+  }
+  else
+  {
+    mainHeight = MAIN_W_SCALAR * winHeight;
+    mainWidth = mainHeight;
+  }
+  $("body").css("width",  mainWidth);
+  // $(".main_content").css("width",  mainWidth);
+  $(".main_content").css("height", mainHeight);
+
+}    
