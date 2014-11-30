44b5d7c436c10156823840eae3adc563f9d112fd
GeorgePTVS <gpolchin@truevisionsys.com>
2014-11-30 13:00:11 -0800

    programmatic width and height based on window. TODO: use window resize fn to do it.

--- webroot/app/main.js
+++ webroot/app/main.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
   // alert('Document Ready' );
+    var winWidth  = $( window ).width();
+    var winHeight = $( window ).height();
+    
+    // set main_content width rel to window and height rel to width
+    var MAIN_W_SCALAR = 0.7;
+    var mainWidth  = MAIN_W_SCALAR * winWidth;
+    var mainHeight = mainWidth;
+    
+    // have to account for aspect ratio.  
+    if ( winWidth < winHeight )
+    {
+      mainWidth = MAIN_W_SCALAR * winWidth;
+      mainHeight = mainWidth;
+    }
+    else
+    {
+      mainHeight = MAIN_W_SCALAR * winHeight;
+      mainWidth = mainHeight;
+    }
+    $("body").css("width",  mainWidth);
+    // $(".main_content").css("width",  mainWidth);
+    $(".main_content").css("height", mainHeight);
+    alert('Document Ready. winWidth = ' + winWidth + ' winHeight = ' + winHeight + ' mainWidth; ' + mainWidth + ' mainHeight: ' + mainHeight);
     $("button").on('click', function() {
     alert("Hello World");
     } );
