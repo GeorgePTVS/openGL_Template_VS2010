8eb902093b723e919d9c22fec7e5c15d3aa0af43
GeorgePTVS <gpolchin@truevisionsys.com>
2014-12-11 23:01:06 -0800

    canvas height breaks layout

--- webroot/app/flakeyCanvas.js
+++ webroot/app/flakeyCanvas.js
@@ -5,10 +5,9 @@ $(document).ready(function(){
   $(window).resize(onWindowResize);
   onWindowResize();  
   $(".colorChooserDiv").on('click', function() {
-//  alert("Flakey color button. id=" + this.id + " hex color = " + this.style.background-color);
 
   var newColor = $(this).css("background-color");
-  alert("Flakey color button. hex color = " + newColor);
+//  alert("Flakey color button. hex color = " + newColor);
   $("#yellow").css("background-color", newColor);
   } );
 
