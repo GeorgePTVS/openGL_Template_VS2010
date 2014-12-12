65aa221114d55d59ba139c3e4b2149d56a5f41f8
GeorgePTVS <gpolchin@truevisionsys.com>
2014-12-11 22:58:12 -0800

    yay getting color in function and using it.

--- webroot/app/flakeyCanvas.js
+++ webroot/app/flakeyCanvas.js
@@ -5,7 +5,11 @@ $(document).ready(function(){
   $(window).resize(onWindowResize);
   onWindowResize();  
   $(".colorChooserDiv").on('click', function() {
-  alert("Flakey color button. id=" + $(this).data("color"));
+//  alert("Flakey color button. id=" + this.id + " hex color = " + this.style.background-color);
+
+  var newColor = $(this).css("background-color");
+  alert("Flakey color button. hex color = " + newColor);
+  $("#yellow").css("background-color", newColor);
   } );
 
 });
