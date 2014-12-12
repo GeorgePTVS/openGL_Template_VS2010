fefa06e0802f3cb4844251b89ee3747146573103
GeorgePTVS <gpolchin@truevisionsys.com>
2014-12-11 23:37:48 -0800

    highlight border inside color selectors

--- webroot/app/flakeyCanvas.js
+++ webroot/app/flakeyCanvas.js
@@ -11,4 +11,24 @@ $(document).ready(function(){
   $("#yellow").css("background-color", newColor);
   } );
 
+
+// put a solid border around color chooser during hover.  Have to manip size since border starts as 0.  
+$( ".colorChooserDiv" ).hover(
+  // TODO Use addClass removeClass (or toggleClass) in each of these fns)
+  function() {
+    var tempBorder = 5;  // px
+    var oldWidth  = $(this).width();
+    var oldHeight = $(this).height();
+    $(this).css("width",  oldWidth  - tempBorder*2);
+    $(this).css("height", oldHeight - tempBorder*2);
+    $(this).css("border", tempBorder + "px solid gray");
+  }, function() {
+    var tempBorder = 5;  // px
+    var oldWidth  = $(this).width();
+    var oldHeight = $(this).height();
+    $(this).css("width",  oldWidth  + tempBorder*2);
+    $(this).css("height", oldHeight + tempBorder*2);
+    $(this).css("border", "0");
+  }
+);  
 });
