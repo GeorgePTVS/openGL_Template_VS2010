201ce2502413c79c3c2bae74eb6b936c3f31c1b5
GeorgePTVS <gpolchin@truevisionsys.com>
2014-12-13 23:20:54 -0800

    shapes array.  Use shape as object, change its properties for each new shape and push_back on shape.

--- webroot/app/flakeyCanvas.js
+++ webroot/app/flakeyCanvas.js
@@ -1,174 +1,161 @@
-$(document).ready(function(){
-  
-  $(window).resize(onWindowResize );
-  onWindowResize();  
-  
+$(document).ready(function () {
+
+  $(window).resize(onWindowResize);
+  onWindowResize();
+
   // --------------------------
   // -- Color chooser
-  // --------------------------  
-  $(".colorChooserDiv").on('click', function() {
-
-  var newColor = $(this).css("background-color");
-  $("#yellow").css("background-color", newColor);
-  } );
-
-
-// put a solid border around color chooser during hover.  Have to manipulate size since border starts as 0.  
-$( ".colorChooserDiv" ).hover(
-  // TODO Use addClass removeClass (or toggleClass) in each of these fns)
-  function() {
-    // var $colorChooserBorder = 5;  // px
-    var oldWidth  = $(this).width();
-    var oldHeight = $(this).height();
-    $(this).css("width",  oldWidth  - $colorChooserBorder*2);
-    $(this).css("height", oldHeight - $colorChooserBorder*2);
-    $(this).css("border", $colorChooserBorder + "px solid gray");
-  }, function() {
-    // var $colorChooserBorder = 5;  // px
-    var oldWidth  = $(this).width();
-    var oldHeight = $(this).height();
-    $(this).css("width",  oldWidth  + $colorChooserBorder*2);
-    $(this).css("height", oldHeight + $colorChooserBorder*2);
-    $(this).css("border", "0");
-  }
- );  
-
- 
   // --------------------------
-  // -- Canvas 
-  // --------------------------  
-  
+  $(".colorChooserDiv").on('click', function () {
+
+    var newColor = $(this).css("background-color");
+    $("#yellow").css("background-color", newColor);
+  });
+
+  // put a solid border around color chooser during hover.  Have to manipulate size since border starts as 0.
+  $(".colorChooserDiv").hover(
+    // TODO Use addClass removeClass (or toggleClass) in each of these fns)
+    function () {
+    // var $colorChooserBorder = 5;  // px
+    var oldWidth = $(this).width();
+    var oldHeight = $(this).height();
+    $(this).css("width", oldWidth - $colorChooserBorder * 2);
+    $(this).css("height", oldHeight - $colorChooserBorder * 2);
+    $(this).css("border", $colorChooserBorder + "px solid gray");
+  }, function () {
+    // var $colorChooserBorder = 5;  // px
+    var oldWidth = $(this).width();
+    var oldHeight = $(this).height();
+    $(this).css("width", oldWidth + $colorChooserBorder * 2);
+    $(this).css("height", oldHeight + $colorChooserBorder * 2);
+    $(this).css("border", "0");
+  });
+
+  // --------------------------
+  // -- Canvas
+  // --------------------------
+
   // globals?
   var $textX = 50;
   var $textY = 50;
-  
-  var defaults = {
-    mode: 1,
-    title: 'Awesome'
-  };
-  var shapeDefault = {
-    mouseXY: { x:0, y:0 },
-    scale: 1.0,
-    color: '#440000',
-    title: 'Awesome'
-  };
-  var $shapes = new Array( shapeDefault );
-  console.log( "$shapes.length = " + $shapes.length + "... props: " + $shapes[0].mouseXY.x + " " + $shapes[0].mouseXY.y + " " + $shapes[0].scale + " " + $shapes[0].color + " " + $shapes[0].title );
 
-  
-  
+  var defaults = {
+    mode : 1,
+    title : 'Awesome'
+  };
+  var shape = {
+    mouseXY : {
+      x : 0,
+      y : 0
+    },
+    scale : 1.0,
+    color : '#440000',
+    title : 'Awesome',
+    type : 'square'
+  };
+  var $shapes = new Array(shape);
+  for (var i = 0; i < $shapes.length; i++)
+  {
+    console.log("$shapes.length = " + $shapes.length + "... props: " + $shapes[i].mouseXY.x + " " + $shapes[i].mouseXY.y + " " + $shapes[i].scale + " " + $shapes[i].color + " " + $shapes[i].title + " " + $shapes[i].type);
+  }
   // // --------------------------
   // // -- Canvas classes
-  // // -------------------------- 
-// // http://jsperf.com/jquery-class-create-vs-pure-js-function/3  
-// <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js">
-// </script>
-// <script src="http://digg.googlecode.com/files/Class-0.0.2.js">
-// </script>
-// <script src="//ajax.googleapis.com/ajax/libs/prototype/1/prototype.js"></script>
- 
-// <script>
+  // // --------------------------
+  // // http://jsperf.com/jquery-class-create-vs-pure-js-function/3
+  // <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js">
+  // </script>
+  // <script src="http://digg.googlecode.com/files/Class-0.0.2.js">
+  // </script>
+  // <script src="//ajax.googleapis.com/ajax/libs/prototype/1/prototype.js"></script>
+
+  // <script>
   // Benchmark.prototype.setup = function() {
-    // var result = '';
+  // var result = '';
   // };
-// </script>
-  
+  // </script>
+
   // var Animal = Class.create({
-    // init: function(name, sound) {
-      // this.name = name;
-      // this.sound = sound;
-    // },
-    // speak: function() {
-      // result = (this.name + " says: " + this.sound + "!");
-    // }
+  // init: function(name, sound) {
+  // this.name = name;
+  // this.sound = sound;
+  // },
+  // speak: function() {
+  // result = (this.name + " says: " + this.sound + "!");
+  // }
   // });
 
   // var cat = new Animal('Kitty', 'Meow');
   // cat.speak();
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
   // $canvass and $ctx are defined in globals.js and are updated in window resizing
-  
-  
+
+
   // --------------------------
   // -- Canvas draw shape
-  // --------------------------  
-  function addShape()
-  {
-  }
+  // --------------------------
+  function addShape() {}
   // --------------------------
   // -- Canvas mouse
-  // --------------------------  
-  
-  function getMousePos(canvas, event)
-  {
+  // --------------------------
+
+  function getMousePos(canvas, event) {
     // can/should also use event.pageX, event.scrollX?  event.screenX?
     var mouseX = event.clientX - canvas.offset().left;
     var mouseY = event.clientY - canvas.offset().top;
     return {
-        x: mouseX,
-        y: mouseY };
-  }  
-  
-  var $mouseXY = { x:0, y:0 };
-  
-  $canvass.on('click', function() {
+      x : mouseX,
+      y : mouseY
+    };
+  }
+
+  var $mouseXY = {
+    x : 0,
+    y : 0
+  };
+
+  $canvass.on('click', function () {
     addShape();
   });
 
-  $canvass.on('mousemove', function(e) {
+  $canvass.on('mousemove', function (e) {
     $mouseXY = getMousePos($canvass, e);
   });
-
 
   // -----------------------------------------
   // Canvas animation/draw loop
   // -----------------------------------------
 
-    function update() {
+  function update() {
     // animation update
-      // could read mouse here. 
-      // $textX += 2;
-      // $textY += 2;
-    }
+    // could read mouse here.
+    // $textX += 2;
+    // $textY += 2;
+  }
 
-    function clearCanvas() {
-      $ctx.clearRect(0, 0, $canvass.width(), $canvass.height());  
-    }
-    
-    function drawScene() {
-    }
+  function clearCanvas() {
+    $ctx.clearRect(0, 0, $canvass.width(), $canvass.height());
+  }
 
-    function drawMouse() {
-      $ctx.fillStyle = "#FFF";
-      $ctx.fillText("Hello WWWorld!", $mouseXY.x, $mouseXY.y);
-    }
+  function drawScene() {}
 
-    function draw() {
-      clearCanvas();
-      // drawScene();
-      drawMouse();
-    }
+  function drawMouse() {
+    $ctx.fillStyle = "#FFF";
+    $ctx.fillText("Hello WWWorld!", $mouseXY.x, $mouseXY.y);
+  }
 
-    var FPS = 30;
-    setInterval(function() {
+  function draw() {
+    clearCanvas();
+    // drawScene();
+    drawMouse();
+  }
+
+  var FPS = 30;
+  setInterval(function () {
     // this is for timer-driven drawing. There is also event-driven (e.g. mousemove)
-      update();
-      draw();
-    }, 1000/FPS);
-
+    update();
+    draw();
+  }, 1000 / FPS);
 
 });
+ 
